f66565638d5882411531a65e9032e2d1e39859b0
Alex Rybachev <rybachev.as@gmail.com>
2021-05-14 17:48:28 +0400

    Create TaskViewController

---
+++ MyCoreDataApp/TaskViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TaskViewController.swift
+//  MyCoreDataApp
+//
+//  Created by Aleksandr Rybachev on 14.05.2021.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
